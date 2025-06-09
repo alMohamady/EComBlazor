28d2c4b0c91b4119582182f60a7ffcff385aa25e
mohamady-div <major_2047@hotmail.com>
2025-06-09 20:49:56 +0300

    Lesson 22 c

--- EComBlazor.db/Services/ServiceContainer.cs
+++ EComBlazor.db/Services/ServiceContainer.cs
@@ -1,15 +1,19 @@
 ﻿using EComBlazor.db.Base;
 using EComBlazor.db.Contexts;
 using EComBlazor.db.Entities;
+using EComBlazor.db.Entities.Identity;
 using EComBlazor.db.Repos;
 using EComBlazor.lib.Base;
 using EComBlazor.lib.Exceptions;
 using EComBlazor.lib.Services;
 using EntityFramework.Exceptions.SqlServer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +40,40 @@ namespace EComBlazor.db.Services
             services.AddScoped<IGenralRepo<Category>, GenralRepo<Category>>();
             services.AddScoped<IGenralRepo<Product>, GenralRepo<Product>>();
             services.AddScoped(typeof(IAppLoger<>), typeof(SerilogAppAdapter<>));
+
+            services.AddDefaultIdentity<AppUser>(options =>
+            {
+                options.SignIn.RequireConfirmedPhoneNumber = true;
+                options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+            }).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    RequireExpirationTime = true,
+                    ValidateIssuerSigningKey = true, 
+                    ValidIssuer = conf["JWT:Issuer"], 
+                    ValidAudience = conf["JWT:Audience"], 
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(conf["JWT:Key"]!))
+                };
+            });
+
             return services;
         }
 
